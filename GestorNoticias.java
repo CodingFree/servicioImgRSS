7bbfb9790d810ed56d29bf9bd355e9dead2dddc3
Adrian <madrid.crespo@gmail.com>
2015-04-24 01:00:09 +0200

    Agregado parser

--- GestorNoticias.java
+++ GestorNoticias.java
@@ -1,86 +1,85 @@
 package servicioImgRSS;
 
-import java.io.InputStream;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 public class GestorNoticias {
 
 	public GestorCategorias gc;
 
-
 	public GestorNoticias(String iniFile) {
-		gc = new GestorCategorias( iniFile );
+		gc = new GestorCategorias(iniFile);
 	}
-	
-	public String getNoticiasXML(String sCodCat) throws Exception {
-		XMLNoticiasHandler  nhXML  = null;
 
-//		JSONNoticiasHandler nhJSON = null;
+	public String getNoticiasXML(String sCodCat) throws Exception {
+		XMLNoticiasHandler nhXML = null;
+
+		// JSONNoticiasHandler nhJSON = null;
 
 		// Enacabezado del XML de salida
 		String result = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<noticias>";
 
 		// Crear Parser SAX
+		SAXParserFactory factoria = SAXParserFactory.newInstance();
+		factoria.setNamespaceAware(true);
+		SAXParser parser= factoria.newSAXParser();
 
 		// Recorre la lista de servicios WEB que ofrecen noticias de la
 		// categoría pasada como parámetro
-		for(InfoServicioWeb iw : gc.getInfoCategoria(sCodCat)) {
-		
-			if ( iw.getType().equals( "XML" ) ) // Solo los servicios XML
-			{	
-				// Procesar URL
-				
-				//-------------
-				//::::-----::::
-				//-------------
-				
-			}
-			else  // Suponemos que es JSON
-			{
-				// Práctica 4
-			}
-		}
-		
-		result += "</noticias>"; // Cierre del XML de salida
+		for (InfoServicioWeb iw : gc.getInfoCategoria(sCodCat)) {
+			System.out.println("La url es: "+iw.getURL());
 
+			if (iw.getType().equals("XML")) // Solo los servicios XML
+			{
+				nhXML = new XMLNoticiasHandler();
+				parser.parse(iw.getURL(), nhXML);
+				result += nhXML.getResult();
+			}
+//			} else // Suponemos que es JSON
+//			{
+//				// Práctica 4
+//			}
+		}
+
+		result += "</noticias>"; // Cierre del XML de salida
+		System.out.println(result);
 		return result;
 	}
-
 
 	public String getNoticiasHTML() {
 		return "";
 	}
 
-
 	public static void main(String[] args) {
 
-		if ( args.length == 0 )
-		{
+		if (args.length == 0) {
 			System.out.println("Falta el codigo de categoria...");
-			System.exit(0);  
+			System.exit(0);
+		}else{
+			System.out.println("La categoría es: "+ args[0]);
 		}
-		
-		GestorNoticias gn = new GestorNoticias( null );
-			
-        FileOutputStream ficheroSalida;
-        PrintStream flujoSalida;
-        
-        try
-        {
-            ficheroSalida = new FileOutputStream( "XML_transform.xml" );
-            flujoSalida   = new PrintStream(ficheroSalida);
 
-			String XML_Transformado = gn.getNoticiasXML( args[0]);
-    			
-            flujoSalida.println ( XML_Transformado );
-            flujoSalida.close();
-        }
-        catch (Exception e)
-        {
+		GestorNoticias gn = new GestorNoticias(null);
+
+		FileOutputStream ficheroSalida;
+		PrintStream flujoSalida;
+
+		try {
+			ficheroSalida = new FileOutputStream("XML_transform.xml");
+			flujoSalida = new PrintStream(ficheroSalida);
+
+			String XML_Transformado = gn.getNoticiasXML(args[0]);
+
+			flujoSalida.println(XML_Transformado);
+			flujoSalida.close();
+			System.out.println("Salida exitosa");
+		} catch (Exception e) {
 			e.printStackTrace();
-            System.err.println ("Error writing to file");
-        }
+			System.err.println("Error writing to file");
+		}
 	}
 
 }
--- XMLNoticiasHandler.java
+++ XMLNoticiasHandler.java
@@ -1,25 +1,61 @@
 package servicioImgRSS;
 
 import java.io.IOException;
+import java.util.List;
 
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
+import org.xml.sax.*;
 import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.parsers.SAXParserFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-
-public class XMLNoticiasHandler extends DefaultHandler {
+public class XMLNoticiasHandler extends DefaultHandler {	
 
 	private String result;
 	
 	public XMLNoticiasHandler() {
-		// Constructor de la clase
+		this.result = "";
 	}
 
 	// Demás metodos del Parser SAX
 	
+    public void startElement(String espacioNombres, String nomLocal, String nomCompleto, Attributes atrs) {
+    	switch(nomCompleto){
+    		case "rss":
+    			this.result += "<rss>";
+    			break;
+    	}
+    	//System.out.println("Nombre local:" + nomLocal + " Nombre completo:" + nomCompleto );
+		
+		for ( int i = 0; i < atrs.getLength(); i++) {
+			//System.out.println("    ATRIB. Nombre local: " + atrs.getLocalName(i) );
+			//System.out.println("    ATRIB. Tipo:         " + atrs.getType(i) );
+			//System.out.println("    ATRIB. Valor:        " + atrs.getValue(i) );
+		}
+    }
+    public void endElement(String espacio, String nomLocal, String nomCompleto) {
+    	switch(nomCompleto){
+		case "rss":
+			this.result += "</rss>";
+			break;
+		}
+    }
+    public void characters_b(char[] ch, int inicio, int longitud) {
+		String cad = new String( ch, inicio, longitud);
+		System.out.println("    Caracteres: " + cad );
+		System.out.println("         Inicio:" + inicio + " Longitud:" + longitud );
+    }
+
+    public void error(SAXParseException exc) throws SAXException {
+		mostrarError( exc, "Se encontró un error");
+    }
+    public void fatalError(SAXParseException exc) throws SAXException {
+		mostrarError( exc, "Se encontró un error fatal");
+    }
+    private void mostrarError( SAXParseException exc, String aviso )  throws SAXException {
+		System.out.println( aviso + ".  Línea:    " + exc.getLineNumber() );
+		System.out.println( "URI:     " + exc.getSystemId() );
+		System.out.println( "Mensaje: " + exc.getMessage() );
+		throw new SAXException(aviso);//
+    }
+    
 	public String getResult() {
 	    return result.toString();
 	}
