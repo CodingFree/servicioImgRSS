a479ffef1050f98ce08ff79d7d64f4dec27c3b1f
Adrian <madrid.crespo@gmail.com>
2015-05-11 14:06:37 +0200

    Ya parsea entradas, solo faltaria el formato de salida

--- JSONNoticiasHandler.java
+++ JSONNoticiasHandler.java
@@ -3,7 +3,13 @@
  */
 package servicioImgRSS;
 
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.util.List;
+
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * @author Tyrion
@@ -12,12 +18,55 @@ import com.google.gson.Gson;
 public class JSONNoticiasHandler {
 
 	private String result = "";
-	
+
 	/**
 	 * 
 	 */
-	public JSONNoticiasHandler(String url) {
-		Gson gson = new Gson();
+	public JSONNoticiasHandler(String sURL) {
+		try {
+			Reader reader = new InputStreamReader(new URL(sURL).openStream(),  "UTF-8");
+			if (reader.ready()) {
+				Gson gson = new GsonBuilder().create();
+				Response obj = gson.fromJson(reader, Response.class);
+			    for (int i = 0; i < obj.responseData.feed.entries.size(); i++) {
+				System.out.println(obj.responseData.feed.entries.get(i).title);
+			    }
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+	}
+
+	private class Response { // This class should match your json object
+								// structure
+		private String responseDetails = null;
+		private int responseStatus;
+		private responseData responseData;
+		//private List<Item> item; // This is for the inner array
+
+		@Override
+		public String toString() {
+			return responseData + " - " + responseStatus + " (" + responseDetails + ")";
+		}
+	}
+
+	private class responseData { // This is the inner array class
+		private Feed feed;
+	}
+	
+	private class Feed{
+		private String title;
+		private List<Entry> entries;		
+	}
+	
+	private class Entry{
+		private String title;
+		private String publishedDate;
+		private String contentSnippet;
+		private String link; // Guid?
+		//private String category;
+		
+		
 	}
 
 }
