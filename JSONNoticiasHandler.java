6bc76fc9444430e95e169b676f3266b3ba0bc0ce
Adrian <madrid.crespo@gmail.com>
2015-05-11 11:34:04 +0200

    Manejador de JSON

---
+++ JSONNoticiasHandler.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package servicioImgRSS;
+
+/**
+ * @author Tyrion
+ *
+ */
+public class JSONNoticiasHandler {
+
+	/**
+	 * 
+	 */
+	public JSONNoticiasHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
